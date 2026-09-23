7cb215b598bbfe4ad50033e28cd87f6e5f714d28
ILBOUDO Christian <ilboudochris79@gmail.com>
2026-09-23 05:11:22 +0000

    Refactor login function to use a message variable
    Store the login message in a variable for clarity.

--- login.js
+++ login.js
@@ -2,3 +2,5 @@ function login() {
   console.log("Connexion utilisateur");
 }
 Soyez plus explicite
+-----------------------------------------------------------------------------------------------------------------
+Pour rendre le code plus explicite, peux-tu stocker le message dans une variable messageConnexion avant de l'afficher ?
