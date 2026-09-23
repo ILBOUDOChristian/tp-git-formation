9e869be013ed60999f48e1e4f532975542b62f16
ILBOUDO Christian <ilboudochris79@gmail.com>
2026-09-23 05:13:54 +0000

    Update login.js

--- login.js
+++ login.js
@@ -3,4 +3,4 @@ function login() {
 }
 Soyez plus explicite
 -----------------------------------------------------------------------------------------------------------------
-Pour rendre le code plus explicite, peux-tu stocker le message dans une variable messageConnexion avant de l'afficher ?
+
